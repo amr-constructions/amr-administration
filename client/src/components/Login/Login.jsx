660b13914e11df3042589124c0d664433367c395
Mohammed Azhar <azharakbar@gmail.com>
2020-08-15 19:35:14 +0530

    [client] [login] :: Basic Form Layout

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,28 +1,79 @@
-import { Card, Col, Row } from 'antd';
-
+import { EyeInvisibleOutlined, EyeTwoTone, LockOutlined, LoginOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Card, Col, Form, Input, Row, Typography } from 'antd';
 import React from 'react';
 import logo from '../../assets/img/amr_logo_black.png';
 import './Login.css';
 
+const { Title } = Typography;
+
 function Login() {
   return (
     <Row
-      className="amr-login"
+      className="amr-login-container"
       justify="center"
       align="middle"
-      style={{
-        minHeight: '100vh',
-      }}
     >
       <Col>
-        <Card
-          style={{
-            width: '50rem',
-          }}
-        >
-          <center>
+        <Card className="amr-login-card">
+          <div className="amr-logo-container">
             <img src={logo} className="logo" alt="AMR Constructions Logo" title="AMR Constructions" />
-          </center>
+          </div>
+
+          <Title
+            className="amr-login-form-title"
+            level={3}
+          >
+            Admin Console
+          </Title>
+
+          <Form
+            name="amr-login-form"
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Username cannot be empty!',
+                },
+              ]}
+            >
+              <Input
+                size="large"
+                placeholder="Username"
+                prefix={<UserOutlined />}
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Password cannot be empty!',
+                },
+              ]}
+            >
+              <Input.Password
+                size="large"
+                placeholder="Password"
+                prefix={<LockOutlined />}
+                iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+              />
+            </Form.Item>
+
+            <Form.Item className="amr-login-submit">
+              <Button
+                type="primary"
+                size="large"
+                icon={<LoginOutlined />}
+                htmlType="submit"
+              >
+                Login!
+              </Button>
+            </Form.Item>
+
+          </Form>
         </Card>
       </Col>
     </Row>
