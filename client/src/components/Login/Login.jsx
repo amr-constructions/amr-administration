dcf150ebb1348c5456eb51245296e7bdb4acadda
Mohammed Azhar <azharakbar@gmail.com>
2020-08-15 05:39:03 +0530

    [client] [login] :: Login form header

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,8 +1,31 @@
+import { Card, Col, Row } from 'antd';
+
 import React from 'react';
+import logo from '../../assets/img/amr_logo_black.png';
+import './Login.css';
 
 function Login() {
   return (
-    <h1> Login </h1>
+    <Row
+      className="amr-login"
+      justify="center"
+      align="middle"
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Col>
+        <Card
+          style={{
+            width: '50rem',
+          }}
+        >
+          <center>
+            <img src={logo} className="logo" alt="AMR Constructions Logo" title="AMR Constructions" />
+          </center>
+        </Card>
+      </Col>
+    </Row>
   );
 }
 
