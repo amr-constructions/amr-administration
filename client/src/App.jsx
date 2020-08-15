5454f6b7e21afd069d76ce50bc24492357acaee3
Mohammed Azhar <azharakbar@gmail.com>
2020-08-16 02:29:03 +0530

    [client] [router] :: Adding routes matched with paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
