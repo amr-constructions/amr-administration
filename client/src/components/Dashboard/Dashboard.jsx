4cf1531ad3e356a7951c425a3fcb0caa11d9cf58
Mohammed Azhar <azharakbar@gmail.com>
2020-08-17 23:34:27 +0530

    [client] [dashboard] :: Converting class component to function comp

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,65 +1,56 @@
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import { Layout } from 'antd';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import SideNav from '../SideNav/SideNav';
 import './Dashboard.css';
 
 const { Header, Content, Footer } = Layout;
 
-class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collapsed: false,
-    };
+const Dashboard = ({ history }) => {
+  const [ state, setState ] = useState({
+    collapsed: false,
+  });
 
-    this.currentYear = new Date().getFullYear();
-    this.toggle = this.toggle.bind(this);
-    this.gotoLocation = this.gotoLocation.bind(this);
-  }
-
-  gotoLocation(to) {
+  function gotoLocation(to) {
     if (to) {
-      const { history } = this.props;
       history.push(to);
     }
   }
 
-  toggle() {
-    this.setState((prevState) => ({
+  const toggle = () => {
+    setState((prevState) => ({
       ...prevState,
       collapsed: !prevState.collapsed,
     }));
-  }
+  };
 
-  render() {
-    const { collapsed } = this.state;
+  const currentYear = new Date().getFullYear();
+  const { collapsed } = state;
 
-    return (
-      <Layout className="dashboard-layout-container">
-        <SideNav collapsed={collapsed} navigate={this.gotoLocation} />
-        <Layout className={`dashboard-layout ${collapsed ? 'side-nav-collapsed' : ''}`}>
-          <Header className="dashboard-header dashboard-background">
-            {
+  return (
+    <Layout className="dashboard-layout-container">
+      <SideNav collapsed={collapsed} navigate={gotoLocation} />
+      <Layout className={`dashboard-layout ${collapsed ? 'side-nav-collapsed' : ''}`}>
+        <Header className="dashboard-header dashboard-background">
+          {
               React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
                 className: 'dashboard-side-nav-trigger',
-                onClick: this.toggle,
+                onClick: toggle,
               })
             }
-            Admin Dashboard
-          </Header>
-          <Content className="dashboard-main-content dashboard-background">
-            Content
-          </Content>
-          <Footer className="dashboard-footer">
-            {`©${this.currentYear} Created by Azanay Tech`}
-          </Footer>
-        </Layout>
+          Admin Dashboard
+        </Header>
+        <Content className="dashboard-main-content dashboard-background">
+          Content
+        </Content>
+        <Footer className="dashboard-footer">
+          {`©${currentYear} Created by Azanay Tech`}
+        </Footer>
       </Layout>
-    );
-  }
-}
+    </Layout>
+  );
+};
 
 Dashboard.propTypes = {
   history: PropTypes.shape({
