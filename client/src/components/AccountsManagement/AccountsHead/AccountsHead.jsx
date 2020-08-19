98856726784a810d290a6d908702c9d8343faf38
Mohammed Azhar <azharakbar@gmail.com>
2020-08-19 14:25:05 +0530

    [client] [dashboard] [accounts_head] :: Basic Table Layout

--- client/src/components/AccountsManagement/AccountsHead/AccountsHead.jsx
+++ client/src/components/AccountsManagement/AccountsHead/AccountsHead.jsx
@@ -1,6 +1,59 @@
 import { HomeOutlined, MoneyCollectTwoTone, UserOutlined } from '@ant-design/icons';
+import { Table } from 'antd';
 import React from 'react';
 import NavigationPath from '../../NavigationPath/NavigationPath';
+import './AccountsHead.css';
+
+const columns = [
+  {
+    title: '#',
+    dataIndex: 'id',
+    render: (text) => <span className="table_slno">{text}</span>,
+  },
+  {
+    title: 'Account Head Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Account Type',
+    dataIndex: 'type',
+  },
+  {
+    title: 'Operational Balance',
+    className: 'column-money',
+    dataIndex: 'op_bal',
+    align: 'right',
+  },
+  {
+    title: 'Current Balance',
+    className: 'column-money',
+    dataIndex: 'curr_bal',
+    align: 'right',
+  },
+  {
+    title: 'Created On',
+    dataIndex: 'created_on',
+  },
+  {
+    title: 'Actions',
+    dataIndex: 'actions',
+    fixed: 'right',
+    width: 100,
+  },
+];
+
+const data = [
+  {
+    key: 1,
+    id: '1',
+    name: 'Supervisor\'s account',
+    type: 'Cash In Hand',
+    op_bal: '0',
+    curr_bal: '0',
+    created_on: new Date().getDate(),
+    actions: 'To Do',
+  },
+];
 
 const navigationPath = [
   {
@@ -24,6 +77,16 @@ const navigationPath = [
 const AccountsHead = () => (
   <div>
     <NavigationPath path={navigationPath} />
+    <Table
+      columns={columns}
+      dataSource={data}
+      bordered
+      title={() => 'List Of Account Heads'}
+      className="accountsHead_Table"
+      pagination={{
+        showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} ${total > 1 ? 'items' : 'item'}`,
+      }}
+    />
   </div>
 );
 
