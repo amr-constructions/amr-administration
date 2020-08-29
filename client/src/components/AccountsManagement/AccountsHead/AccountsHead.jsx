b45d0301f8f036030c8944562d3be4de2c338094
Mohammed Azhar <azharakbar@gmail.com>
2020-08-29 17:08:51 +0530

    [client] [dashboard] [accounts_head] [edit_account_head] :: Submission and error handling for Edit Account Head

--- client/src/components/AccountsManagement/AccountsHead/AccountsHead.jsx
+++ client/src/components/AccountsManagement/AccountsHead/AccountsHead.jsx
@@ -124,6 +124,58 @@ const AccountsHead = () => {
     });
   };
 
+  const updateAccountForm = async (values) => {
+    setState((prevState) => ({
+      ...prevState,
+      modalSubmit: true,
+    }));
+
+    const response = await Services[Constants.ACCOUNTS_MGMT.UPDATE_ACCOUNT_HEAD](values);
+    if (response.code !== Constants.SUCCESS) {
+      setState((prevState) => ({
+        ...prevState,
+        tableLoading: false,
+        modalSubmit: false,
+      }));
+
+      message.error(`${response.reason} [${response.debugCode}]`);
+      return;
+    }
+
+    setState((prevState) => ({
+      ...prevState,
+      tableLoading: true,
+    }));
+
+    const idx = state.data.findIndex((item) => item.id === response.data.id);
+    if (idx === -1) {
+      setState((prevState) => ({
+        ...prevState,
+        tableLoading: false,
+        modalSubmit: false,
+      }));
+
+      message.error('Account Head Update Failed [CLIENT]');
+      return;
+    }
+
+    const updatedData = state.data.slice(0);
+    updatedData[idx] = {
+      ...updatedData[idx], ...response.data,
+    };
+
+    setState((prevState) => {
+      message.success('Account Head Updated Successfully');
+      return ({
+        ...prevState,
+        data: updatedData,
+        tableLoading: false,
+        editAccountHeadVisible: false,
+        modalSubmit: false,
+      });
+    });
+  };
+
   return (
     <div>
       <NavigationPath path={navigationPath} />
@@ -158,7 +210,7 @@ const AccountsHead = () => {
         }}
       />
       <NewAccountHeadForm ref={formRef} onSubmit={submitNewAccountForm} state={state} setState={setState} />
-      <EditAccountHeadForm state={state} setState={setState} />
+      <EditAccountHeadForm onSubmit={updateAccountForm} state={state} setState={setState} />
     </div>
   );
 };
