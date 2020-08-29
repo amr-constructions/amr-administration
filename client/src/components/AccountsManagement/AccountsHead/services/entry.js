7e8786088f3be2f2e1b321786c08b8d0cd9ecdfd
Mohammed Azhar <azharakbar@gmail.com>
2020-08-29 17:08:26 +0530

    [client] [dashboard] [accounts_head] [services] :: Service to update account head

--- client/src/components/AccountsManagement/AccountsHead/services/entry.js
+++ client/src/components/AccountsManagement/AccountsHead/services/entry.js
@@ -1,8 +1,10 @@
 import Constants from '../../../../constants/Constants';
 import CreateAccountHead from './createAccountHead';
 import GetAccountHeads from './getAccountHeads';
+import UpdateAccountHeads from './updateAccountHead';
 
 export default {
   [Constants.ACCOUNTS_MGMT.CREATE_ACCOUNT_HEAD]: CreateAccountHead,
   [Constants.ACCOUNTS_MGMT.GET_ACCOUNT_HEADS]: GetAccountHeads,
+  [Constants.ACCOUNTS_MGMT.UPDATE_ACCOUNT_HEAD]: UpdateAccountHeads,
 };
---
+++ client/src/components/AccountsManagement/AccountsHead/services/updateAccountHead.js
@@ -0,0 +1,16 @@
+import Constants from '../../../../constants/Constants';
+import { buildResponseObject, initResponseObject } from '../../../../services/Common';
+
+export default async (data) => {
+  const response = initResponseObject();
+
+  if (!data) {
+    buildResponseObject(response, Constants.ACCOUNTS_MGMT.MODULE, 501);
+  } else {
+    response.data = {
+      ...data,
+    };
+  }
+
+  return response;
+};
