9f323577fbcc4d791de16aedc80a0283c937002f
Mohammed Azhar <azharakbar@gmail.com>
2020-08-30 00:18:05 +0530

    [client] [dashboard] [accounts_head] [services] :: Get transaction list of an account optionally with a time period

--- client/src/components/AccountsManagement/services/entry.js
+++ client/src/components/AccountsManagement/services/entry.js
@@ -1,10 +1,12 @@
 import Constants from '../../../constants/Constants';
 import CreateAccountHead from './createAccountHead';
 import GetAccountHeads from './getAccountHeads';
+import GetAccountTxns from './getAccountTxns';
 import UpdateAccountHeads from './updateAccountHead';
 
 export default {
   [Constants.ACCOUNTS_MGMT.CREATE_ACCOUNT_HEAD]: CreateAccountHead,
   [Constants.ACCOUNTS_MGMT.GET_ACCOUNT_HEADS]: GetAccountHeads,
   [Constants.ACCOUNTS_MGMT.UPDATE_ACCOUNT_HEAD]: UpdateAccountHeads,
+  [Constants.ACCOUNTS_MGMT.GET_ACCOUNT_TXNS]: GetAccountTxns,
 };
