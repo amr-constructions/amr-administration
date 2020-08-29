960e6a38865a8969e45acf9b284a3351ed6ffa19
Mohammed Azhar <azharakbar@gmail.com>
2020-08-29 22:11:31 +0530

    [client] [dashboard] [accounts_head] [account_txns] :: Basic Form Layout

--- client/src/components/AccountsManagement/AccountLedger/AccountLedger.jsx
+++ client/src/components/AccountsManagement/AccountLedger/AccountLedger.jsx
@@ -1,6 +1,10 @@
-import { HomeOutlined, PropertySafetyTwoTone, UserOutlined } from '@ant-design/icons';
-import React from 'react';
+import { HomeOutlined, PropertySafetyTwoTone, SearchOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Card, DatePicker, Form, Select, Space } from 'antd';
+import React, { useState } from 'react';
 import NavigationPath from '../../NavigationPath/NavigationPath';
+import './AccountLedger.css';
+
+const { RangePicker } = DatePicker;
 
 const navigationPath = [
   {
@@ -21,10 +25,65 @@ const navigationPath = [
   },
 ];
 
-const AccountLedger = () => (
-  <div>
-    <NavigationPath path={navigationPath} />
-  </div>
-);
+const AccountLedger = () => {
+  const [ state, setState ] = useState({
+    submit: false,
+    options: [],
+  });
+
+  return (
+    <div>
+      <NavigationPath path={navigationPath} />
+
+      <Card className="account-ledger-search">
+        <Form
+          layout="vertical"
+          hideRequiredMark
+        >
+          <Space size="large" align="end" className="account-ledger-search-form-spacer">
+
+            <Form.Item
+              label="Account"
+              name="account_name"
+              rules={[
+                {
+                  required: true,
+                  message: 'Account Name Must Be Selected!',
+                },
+              ]}
+            >
+              <Select
+                showSearch
+                size="large"
+                placeholder="Account Name"
+                optionFilterProp="children"
+                filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                disabled={state.submit}
+                autoFocus
+                options={state.options}
+              />
+            </Form.Item>
+
+            <Form.Item label="Time Period" name="period">
+              <RangePicker size="large" renderExtraFooter={() => 'Leave Time Period Empty To Get All Transactions'} disabled={state.submit} />
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type="primary"
+                icon={<SearchOutlined />}
+                htmlType="submit"
+                loading={state.submit}
+              >
+                Search
+              </Button>
+            </Form.Item>
+
+          </Space>
+        </Form>
+      </Card>
+    </div>
+  );
+};
 
 export default AccountLedger;
