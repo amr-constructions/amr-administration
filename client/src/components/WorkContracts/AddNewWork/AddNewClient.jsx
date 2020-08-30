7ce439f97b7ee5be54883d2ab0b19ed8a01018d1
Mohammed Azhar <azharakbar@gmail.com>
2020-08-30 17:29:54 +0530

    [client] [dashboard] [add_new_client] :: Modal and form for Add New Client

---
+++ client/src/components/WorkContracts/AddNewWork/AddNewClient.jsx
@@ -0,0 +1,118 @@
+import { CheckOutlined, MobileOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input, Modal } from 'antd';
+import PropTypes from 'prop-types';
+import React, { useRef } from 'react';
+
+const AddNewClient = ({ onSubmit, state, setState }) => {
+  const formRef = useRef(null);
+
+  const { newClientFormVisible: visible } = state;
+
+  const imitateContactNumberInput = (e) => {
+    const { value } = e.target;
+    if (/^\d{0,9}$/.test(value)) {
+      formRef.current.setFieldsValue({
+        contact: value,
+      });
+    } else {
+      formRef.current.setFieldsValue({
+        contact: value.slice(0, -1),
+      });
+    }
+  };
+
+  return (
+    <div>
+      <Modal
+        title="Create New Client"
+        visible={visible}
+        destroyOnClose
+        onCancel={() => {
+          setState((prevState) => ({
+            ...prevState,
+            newClientFormVisible: false,
+          }));
+          formRef.current.resetFields();
+        }}
+        footer={[
+          <Button
+            key={0}
+            type="primary"
+            loading={false}
+            onClick={() => {
+              formRef.current.submit();
+            }}
+            icon={<CheckOutlined />}
+            disabled={false}
+          >
+            Create
+          </Button>,
+        ]}
+      >
+        <Form
+          name="amr-new-client-form"
+          hideRequiredMark
+          layout="vertical"
+          ref={formRef}
+          onFinish={onSubmit}
+        >
+
+          <Form.Item
+            name="clientName"
+            label="Client Name"
+            rules={[
+              {
+                required: true,
+                message: 'Client Name cannot be empty!',
+              },
+            ]}
+          >
+            <Input
+              size="large"
+              placeholder="Client Name"
+              prefix={<UserOutlined />}
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="contact"
+            label="Contact Number"
+            validateTrigger={[ 'onSubmit' ]}
+            rules={[
+              () => ({
+                validator(_, value) {
+                  if (value == null || !value.length) {
+                    return Promise.reject(new Error('Contact Number is mandatory!'));
+                  }
+                  if (!/^\d{9}$/.test(value)) {
+                    return Promise.reject(new Error('Contact Number must contain exactly 9 digits!'));
+                  }
+                  return Promise.resolve();
+                },
+              }),
+            ]}
+          >
+            <Input
+              size="large"
+              placeholder="Contact Number"
+              prefix="+91"
+              addonBefore={<MobileOutlined />}
+              onChange={imitateContactNumberInput}
+            />
+          </Form.Item>
+
+        </Form>
+      </Modal>
+    </div>
+  );
+};
+
+AddNewClient.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  state: PropTypes.shape({
+    newClientFormVisible: PropTypes.bool,
+  }).isRequired,
+  setState: PropTypes.func.isRequired,
+};
+
+export default AddNewClient;
