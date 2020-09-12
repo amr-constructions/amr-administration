a65175ecc1131798c016c9d821a91a44d451e05e
Mohammed Azhar <azharakbar@gmail.com>
2020-09-12 23:52:29 +0530

    [client] [dashboard] [side-nav] [labour_mgmt] [view_labours] :: Component for Individual Labours

--- client/src/components/LabourManagement/ViewAllLabours/ViewAllLabours.jsx
+++ client/src/components/LabourManagement/ViewAllLabours/ViewAllLabours.jsx
@@ -1,6 +1,7 @@
 import { HomeOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
 import React from 'react';
 import NavigationPath from '../../NavigationPath/NavigationPath';
+import ViewIndividualLabours from '../ViewIndividualLabours/ViewIndividualLabours';
 
 const navigationPath = [
   {
@@ -24,6 +25,7 @@ const navigationPath = [
 const ViewAllLabours = () => (
   <div>
     <NavigationPath path={navigationPath} />
+    <ViewIndividualLabours />
   </div>
 );
 
---
+++ client/src/components/LabourManagement/ViewIndividualLabours/ViewIndividualLabours.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ViewIndividualLabours = () => (
+  <div />
+);
+
+export default ViewIndividualLabours;
