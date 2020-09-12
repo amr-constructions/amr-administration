35d3a720873966e61e54c5f5dcc7a9b16e57c11e
Mohammed Azhar <azharakbar@gmail.com>
2020-09-13 00:31:50 +0530

    [client] [dashboard] [side-nav] [labour_mgmt] [individual_labour] :: Table Model

--- client/src/components/LabourManagement/ViewIndividualLabours/ViewIndividualLabours.jsx
+++ client/src/components/LabourManagement/ViewIndividualLabours/ViewIndividualLabours.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import { AppstoreAddOutlined, LoadingOutlined } from '@ant-design/icons';
+import { Table } from 'antd';
+import React, { useState } from 'react';
+import TableTitle from '../../TableTitle/TableTitle';
+import Columns from './models/TableColumns';
 
-const ViewIndividualLabours = () => (
-  <div />
-);
+const ViewIndividualLabours = () => {
+  const [ state, setState ] = useState({
+    data: [],
+    tableLoading: false,
+  });
+
+  const addNewIndividualLabour = () => {
+
+  };
+
+  return (
+    <Table
+      columns={Columns()}
+      dataSource={state.data}
+      bordered
+      title={() => (
+        <TableTitle
+          title="List Of Individual Labours"
+          button={{
+            icon: AppstoreAddOutlined,
+            type: 'primary',
+            label: 'New Individual Labour',
+            onClick: addNewIndividualLabour,
+          }}
+        />
+      )}
+      className="individualLabours_Table"
+      pagination={{
+        showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} ${total > 1 ? 'items' : 'item'}`,
+      }}
+      loading={{
+        indicator: (<LoadingOutlined />),
+        size: 'large',
+        tip: 'Loading Data...',
+        spinning: state.tableLoading,
+      }}
+    />
+  );
+};
 
 export default ViewIndividualLabours;
