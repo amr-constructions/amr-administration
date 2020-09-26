59b86a96c48de6a3ed27b6b13b9f08601255d436
Mohammed Azhar <azharakbar@gmail.com>
2020-09-27 00:23:33 +0530

    [client] [labour_mgmt] [services] :: Service To Update Labour

--- client/src/components/LabourManagement/services/entry.js
+++ client/src/components/LabourManagement/services/entry.js
@@ -1,8 +1,10 @@
 import Constants from '../../../constants/Constants';
 import CreateIndividualLabour from './createIndividualLabour';
 import GetLabours from './getLabours';
+import UpdateLabour from './updateLabour';
 
 export default {
   [Constants.LABOURS_MGMT.GET_LABOURS]: GetLabours,
   [Constants.LABOURS_MGMT.CREATE_INDIVIDUAL_LABOUR]: CreateIndividualLabour,
+  [Constants.LABOURS_MGMT.UPDATE_LABOUR]: UpdateLabour,
 };
